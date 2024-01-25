814e9e77feada7adf002275f5caa0b1ade8a28a3
mateusrcm <mateus.rc.martins@gmail.com>
2024-01-25 11:30:44 -0300

    feat: basic root interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+import { ThemeService } from '../shared/services/theme.service';
+import { ThemeType } from '../shared/models/theme';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NzLayoutModule,
+    NzButtonModule,
+    NzMenuModule,
+    NzIconModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
-export class AppComponent {
-  title = 'my-finances';
+export class AppComponent implements OnInit {
+  constructor(public themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.loadTheme(true);
+  }
+
+  changeTheme(): void {
+    const theme: ThemeType =
+      this.themeService.currentTheme === 'dark' ? 'light' : 'dark';
+
+    this.themeService.changeTheme(theme);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,41 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import en from '@angular/common/locales/en';
 
 import { routes } from './app.routes';
 
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  BulbOutline,
+  BulbFill,
+  HomeOutline,
+  BarChartOutline,
+  TableOutline,
+} from '@ant-design/icons-angular/icons';
+
+registerLocaleData(en);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideNzI18n(en_US),
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(
+      NzIconModule.forChild([
+        BulbOutline,
+        BulbFill,
+        HomeOutline,
+        BarChartOutline,
+        TableOutline,
+      ])
+    ),
+    provideAnimations(),
+  ],
 };
