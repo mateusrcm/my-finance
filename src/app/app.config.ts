0aa80147e8310b62304cfdec6a3eb3c5b3982baa
Mateus Martins <mateus.martins@aiwyn.ai>
2025-01-11 12:28:42 -0300

    feat: upgrade angular to version 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import en from '@angular/common/locales/en';
