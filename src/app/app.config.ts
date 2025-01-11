f6779d4bcd8b6e05589459657c962f4dbe0793c3
Mateus Martins <mateus.martins@aiwyn.ai>
2025-01-11 12:41:25 -0300

    fix: removed deprecated code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import en from '@angular/common/locales/en';
@@ -36,8 +36,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideNzConfig(globalConfig),
     provideNzI18n(en_US),
+    provideHttpClient(),
     importProvidersFrom(FormsModule),
-    importProvidersFrom(HttpClientModule),
     importProvidersFrom(
       NzIconModule.forChild([
         BulbOutline,
